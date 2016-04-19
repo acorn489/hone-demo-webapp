66ee6d905783927381a5c8305589cb62cde822e3
Christian Petrov <wwhhooaahh@gmail.com>
2016-04-19 22:45:24 +0200

    Make app easier to use for experiments
    Make Hone URL, Client ID and Skill ID configurable.

--- src/main.js
+++ src/main.js
@@ -1,59 +1,77 @@
-import authorize from 'oauth2-implicit'
+import authorize from "oauth2-implicit"
 
-$(function() {
-  var token;
-  if (getParameterByName("access_token")) {
-    token = getParameterByName("access_token");
-    $(".authenticate").hide();
-    $(".complete_skill").show();
+let INPUT_IDS = ["honeUrl", "clientId", "skillId"];
+
+$(() => {
+  let token = getParameterByName("access_token");
+  let $authenticate = $(".authenticate");
+  let $completeSkill = $(".complete_skill");
+  restoreInputValues();
+  showButton(token, $completeSkill, $authenticate);
+  $("#defaultHoneUrl").click(handleDefaultHoneUrlClick());
+  $authenticate.click(handleAuthenticateClick());
+  $completeSkill.click(handleCompleteSkillClick(token));
+});
+
+function getParameterByName(name, url) {
+  if (!url) url = window.location.href;
+  name = name.replace(/[\[\]]/g, "\\$&");
+  var regex = new RegExp("[#&]" + name + "(=([^&#]*)|&|#|$)", "i"),
+    results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return '';
+  return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+function showButton(token, $completeSkill, $authenticate) {
+  if (token) {
+    $completeSkill.show().siblings().hide();
   } else {
-    $(".authenticate").show();
-    $(".complete_skill").hide();
+    $authenticate.show().siblings().hide();
   }
+}
 
-  $(".authenticate").click(() => {
-    let credentials = authorize({
-      auth_uri: 'http://localhost:3000/oauth/authorize',
-      client_id: '9b82f0ba722b4f31788cc8ec6f3ff0d2ef8c137ece63dfbe3290ad746f42033a',
+function restoreInputValues() {
+  INPUT_IDS.forEach(id => $("#" + id).val(localStorage.getItem(id)));
+}
+
+function storeInputValues() {
+  INPUT_IDS.forEach(id => localStorage.setItem(id, $("#" + id).val()));
+}
+
+function handleDefaultHoneUrlClick() {
+  return () => $("#honeUrl").val("https://hone-kids.herokuapp.com/");
+}
+
+function handleAuthenticateClick() {
+  return () => {
+    storeInputValues();
+    authorize({
+      auth_uri: $("#honeUrl").val() + '/oauth/authorize',
+      client_id: $("#clientId").val(),
       scope: [],
-      state: {
-        location: window.location
-      }
-    })
-    credentials.run();
-  });
+      state: {location: window.location}
+    }).run();
+  }
+}
 
-  // Ladda.bind( 'button[type=submit]' );
-  $(".complete_skill").click(function(e){
+function handleCompleteSkillClick(token) {
+  return function (e) {
     var el = $(this);
     var l = Ladda.create(this);
     l.start();
     var data = new FormData();
-    data.append("skill_id", 9);
+    data.append("skill_id", $("#skillId").val());
     data.append("access_token", token);
-    fetch("http://localhost:3000/api/v1/complete_skill", {method: "post", body: data})
-      .then(function() {
-        return new Promise(function(resolve, reject) {
-          setTimeout(resolve, 1000);
-        });
-      })
-      .then(function() {
+    fetch($("#honeUrl").val() + "/api/v1/complete_skill", {method: "post", body: data})
+      .then(() => new Promise((resolve, reject) => setTimeout(resolve, 1000)))
+      .then(() => {
         l.stop();
         el.css("background-color", "green");
       })
-      .catch(function() {
+      .catch(() => {
         console.log("error");
         console.log(e);
       });
-  });
-});
-
-function getParameterByName(name, url) {
-    if (!url) url = window.location.href;
-    name = name.replace(/[\[\]]/g, "\\$&");
-    var regex = new RegExp("[#&]" + name + "(=([^&#]*)|&|#|$)", "i"),
-        results = regex.exec(url);
-    if (!results) return null;
-    if (!results[2]) return '';
-    return decodeURIComponent(results[2].replace(/\+/g, " "));
+  }
 }
